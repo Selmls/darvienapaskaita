4cbc26855ca3a5aeb6d162699bfff47c8b66351c
Eimis <Eimis4k@gmail.com>
2023-12-05 22:59:17 -0800

    Committed

--- main.py
+++ main.py
@@ -11,22 +11,20 @@ c = 0
 x = 0
 y = 0
 while True:
+    print("Diapason: ", "From: ", y, "To: ", x or d)
     b = int(input("Enter number: "))
+    if b > x or b < y:
+        print("Number is not in the diapason")
     c += 1
     if b == a:
         print("U won")
         print("Guess count: ", c)
         break
     elif b > a:
-        if b < y:
-            print("Number is not in the diapason")
         x = b
         print("Number is lower")
-        print("Diapason: ", "From: ", y, "To: ", x)
         # break
     elif b < a:
-        if b > x:
-            print("Number is not in the diapason")
         y = b
         print("Number is higher")
-        print("Diapason: ", "From", y, "To: ", (x or d))
+
